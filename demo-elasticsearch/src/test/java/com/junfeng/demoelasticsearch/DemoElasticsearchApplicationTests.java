2155931ce21eabadbe4ac05f7e44b1e35ed16d5f
junfeng <1033110136@qq.com>
2022-06-01 23:48:04 +0800

    add:ES

---
+++ demo-elasticsearch/src/test/java/com/junfeng/demoelasticsearch/DemoElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junfeng.demoelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoElasticsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
