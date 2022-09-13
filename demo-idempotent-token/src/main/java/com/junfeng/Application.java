ced617c0ca4d8bffd00ff2c14b25066fbfec5264
junfeng <tangjunfeng>
2022-09-13 10:50:29 +0800

    接口幂等性示例：防重Token令牌

---
+++ demo-idempotent-token/src/main/java/com/junfeng/Application.java
@@ -0,0 +1,20 @@
+package com.junfeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author junfeng
+ * @Classname Application
+ * @Description
+ * @Date 2022-09-13
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
